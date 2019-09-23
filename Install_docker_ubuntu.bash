f45e98e8145bb3a404bf300e2c93341e79126cf2
Jim Vekemans <JimVekemans@hotmail.com>
2019-09-23 19:59:54 +0200

    Docker for ubuntu / pop-os
    Install script for setting up docker-ce on Ubuntu-based Linux distributions

---
+++ Install_docker_ubuntu.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo groupadd docker
+sudo usermod -aG docker $USER
+systemctl enable docker
+echo "----- YOU SHOULD REBOOT BEFORE USING DOCKER -----"
+
