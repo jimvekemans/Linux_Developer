2837696401c704821bafc446f583e126dea4734e
Jim Vekemans <>
2023-12-20 16:14:41 +0100

    extra line

--- bootloaders/configure_systemd_boot.bash
+++ bootloaders/configure_systemd_boot.bash
@@ -3,3 +3,8 @@
 # This script minimises checks and time spent on the boot process.
 sudo bootctl set-timeout 0
 sudo bootctl update
+
+# This script sets the default boot entry to the one with the highest version number.
+
+# Get the default boot entry.
+default_entry=$(sudo bootctl list | grep -E '^\*.*' | awk '{print $2}')
